b77cb8ee0f27b18abd4621952e278731613c11a0
Pratik Gohil <pr.tikgohil@gmail.com>
2022-08-28 16:52:21 +0530

    added: marketdepth on expand to watchlist

--- src/http/subscribeInstruments/subscribeInstruments.ts
+++ src/http/subscribeInstruments/subscribeInstruments.ts
@@ -1,14 +1,18 @@
+import { IInstrument } from "./../../types/interfaces/instrument.interfaces.types";
 import { constants } from "../../constants/global";
 import HTTP from "../http";
 const { TOKEN } = constants;
 
-export const subscribeInstruments = async (data) => {
+export const subscribeInstruments = async ({
+  instruments,
+  xtsMessageCode = 1501,
+}) => {
   const response = await HTTP.post(
     `${process.env.REACT_APP_API_BASE_URL}/marketdata/instruments/subscription`,
     {
       body: JSON.stringify({
-        instruments: data,
-        xtsMessageCode: 1501,
+        instruments,
+        xtsMessageCode,
       }),
       method: "POST",
       headers: {
