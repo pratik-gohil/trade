eba7dc8588010caf0202766be46f6f03b0320c00
Pratik Gohil <pr.tikgohil@gmail.com>
2022-08-28 19:13:47 +0530

    added: gttorders

---
+++ src/http/getGTTOrders/getGTTOrders.ts
@@ -0,0 +1,18 @@
+import { constants } from "../../constants/global";
+import HTTP from "../http";
+const { CLIENT_ID, TOKEN, USER_ID } = constants;
+
+export const getGTTOrders = async () => {
+  return await HTTP.get({
+    url: `${
+      process.env.REACT_APP_API_BASE_URL
+    }/enterprise/orders/gttorderbook?clientID=${localStorage.getItem(
+      CLIENT_ID
+    )}&userID=${localStorage.getItem(USER_ID)}`,
+    requestOptions: {
+      headers: {
+        Authorization: localStorage.getItem(TOKEN) || "",
+      },
+    },
+  });
+};
