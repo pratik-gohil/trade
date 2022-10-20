4ac3acd58e1088a0af5e2c5022422e26f3c9b914
Pratik Gohil <pr.tikgohil@gmail.com>
2022-10-20 20:25:07 +0530

    added: fundamentals overview api

--- src/http/fundamental/fundamental/fundamental.ts
+++ src/http/fundamental/fundamental/fundamental.ts
@@ -1,2 +1,13 @@
-// REACT_APP_FUNDAMENTAL_BASE_URL
-export const fundamental = () => {};
+import HTTP from "../../http";
+
+export const fundamental_overview = async () => {
+  return await HTTP.post(
+    `http://api.lkp.net.in/commonAPI_test/Fundamental/fundamental/INE009A01021`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${btoa("SW76Q5JIAD:lkp@123")}`,
+      },
+    }
+  );
+};
