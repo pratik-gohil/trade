e1893942b6e5a8265a5e681d72bd6357b62cfb89
Pratik Gohil <pr.tikgohil@gmail.com>
2022-11-03 16:37:20 +0530

    removed unused apis

--- src/http/hdfc_upi/transactionStatusQuery.ts
+++
@@ -1,8 +0,0 @@
-import HTTP from "../http";
-
-export const verify_vpa = async () => {
-  return await HTTP.get({
-    url: `${process.env.REACT_APP_API_HDFC_UPI_BASE_URL}/transactionStatusQuery`,
-    requestOptions: {},
-  });
-};
