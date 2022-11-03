f58b70e4ff8226af61575893b1b60b84fc02de2e
Pratik Gohil <pr.tikgohil@gmail.com>
2022-11-03 17:20:46 +0530

    removed: test-changes

--- src/http/hdfc_upi/createOrder.ts
+++ src/http/hdfc_upi/createOrder.ts
@@ -7,8 +7,6 @@ export const createOrder = async ({
 }) => {
   return await HTTP.get({
     url: `https://backoffice.lkp.net.in:8080/techexcelapi/index.cfm/PGEntry/PGEntry?&Client_Code=${Client_Code}&Bank_UPIID=TradeTrade@hdfcbank&Client_Bank_Acno=${Client_Bank_Acno}&Amount=${Amount}&Client_UPI_ID=&MICR=&UrlUserName=techapi&UrlPassword=TECH@123&UrlDatabase=capsfo&UrlDataYear=2022`,
-    requestOptions: {
-      mode: "no-cors",
-    },
+    requestOptions: {},
   });
 };
