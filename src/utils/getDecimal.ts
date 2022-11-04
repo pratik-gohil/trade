98f11a61c955005d19f65e8e302a313db94ee7ec
Pratik Gohil <pr.tikgohil@gmail.com>
2022-11-04 13:10:08 +0530

    fixed: tick size validation

--- src/utils/getDecimal.ts
+++ src/utils/getDecimal.ts
@@ -1,2 +1,3 @@
 import { toFixedN } from "./toFixedN";
-export const getDecimal = (n) => Number(toFixedN(n - Math.floor(n)));
+// export const getDecimal = (n) => Number(toFixedN(n - Math.floor(n)));
+export const getDecimal = (n) => Number(toFixedN(n).split(".")[1]) || 0;
