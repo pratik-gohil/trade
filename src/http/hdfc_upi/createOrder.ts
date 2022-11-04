62a98008d5848d41783e70c976ab7ce669e5a00b
Pratik Gohil <pr.tikgohil@gmail.com>
2022-11-05 02:09:59 +0530

    fixed: create order api call

--- src/http/hdfc_upi/createOrder.ts
+++ src/http/hdfc_upi/createOrder.ts
@@ -1,12 +1,12 @@
 import HTTP from "../http";
+import { constants } from "../../constants/global";
+const { CLIENT_ID } = constants;
 
-export const createOrder = async ({
-  Client_Code = "16500001",
-  Client_Bank_Acno,
-  Amount,
-}) => {
+export const createOrder = async ({ Client_Bank_Acno, Amount }) => {
   return await HTTP.get({
-    url: `https://backoffice.lkp.net.in:8080/techexcelapi/index.cfm/PGEntry/PGEntry?&Client_Code=${Client_Code}&Bank_UPIID=TradeTrade@hdfcbank&Client_Bank_Acno=${Client_Bank_Acno}&Amount=${Amount}&Client_UPI_ID=&MICR=&UrlUserName=techapi&UrlPassword=TECH@123&UrlDatabase=capsfo&UrlDataYear=2022`,
+    url: `https://backoffice.lkp.net.in:8080/techexcelapi/index.cfm/PGEntry/PGEntry?&Client_Code=${localStorage.getItem(
+      CLIENT_ID
+    )}&Bank_UPIID=TradeTrade@hdfcbank&Client_Bank_Acno=${Client_Bank_Acno}&Amount=${Amount}&Client_UPI_ID=&MICR=&UrlUserName=techapi&UrlPassword=TECH@123&UrlDatabase=capsfo&UrlDataYear=${new Date().getFullYear()}`,
     requestOptions: {},
   });
 };
