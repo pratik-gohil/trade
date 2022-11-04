8d8a2a8c17fb2b642753dd0aad4ea5c9bad427ad
Pratik Gohil <pr.tikgohil@gmail.com>
2022-11-05 02:47:42 +0530

    added: kycmaster api

---
+++ src/http/kycMaster/kycMaster.ts
@@ -0,0 +1,12 @@
+import { constants } from "../../constants/global";
+import HTTP from "../http";
+const { CLIENT_ID } = constants;
+
+export const getKycMaster = async () => {
+  return await HTTP.get({
+    url: `https://backoffice.lkp.net.in:8080/techexcelapi/index.cfm/kycMaster/GetMaster?ClientCode=${localStorage.getItem(
+      CLIENT_ID
+    )}&UrlUserName=techapi&UrlPassword=TECH@123`,
+    requestOptions: {},
+  });
+};
