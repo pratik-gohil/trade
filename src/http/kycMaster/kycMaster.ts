ff173a33151a9143238768d3d3fa961a2cbc87f5
Pratik Gohil <pr.tikgohil@gmail.com>
2022-11-05 15:59:14 +0530

    added: kyc data to redux store

--- src/http/kycMaster/kycMaster.ts
+++ src/http/kycMaster/kycMaster.ts
@@ -6,7 +6,7 @@ export const getKycMaster = async () => {
   return await HTTP.get({
     url: `https://backoffice.lkp.net.in:8080/techexcelapi/index.cfm/kycMaster/GetMaster?ClientCode=${localStorage.getItem(
       CLIENT_ID
-    )}&UrlUserName=techapi&UrlPassword=TECH@123`,
+    )}&UrlUserName=techapi&UrlPassword=TECH@123&UrlDatabase=capsfo&UrlDataYear=${new Date().getFullYear()}`,
     requestOptions: {},
   });
 };
